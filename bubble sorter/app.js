238724fe5323ed65d5c5d537f9bd92b78848239a
Evan Brecht-Curry <starfoxz13@gmail.com>
2019-11-27 16:34:24 -0800

    small tweaks

--- bubble sorter/app.js
+++ bubble sorter/app.js
@@ -5,13 +5,14 @@ document.getElementById("autoSort").addEventListener("click", autoSort);
 document.getElementById("stopAutoSort").addEventListener("click", stopAutoSort);
 
 // global variables
-let box = 25;
+const box = 25;
 let sort;
 let arr = [];
 let arrLength = 0;
 let currentStep = 0;
 let savedArray = [];
-let audio = new Audio('./assets/click.mp3');
+let changedIndex = [];
+const audio = new Audio('./assets/click.mp3');
 
 // handle steps
 let stepsOutput = document.getElementById("steps");
@@ -25,7 +26,10 @@ output.innerHTML = rangeSlider.value;
 rangeSlider.oninput = function() {
   output.innerHTML = this.value;
   arrLength = this.value;
-  generateArray(arrLength)
+  currentStep = 0;
+  changedIndex = 0;
+  stepsOutput.innerHTML = currentStep;
+  generateArray(arrLength);
 };
 
 function generateArray (arrLength) {
@@ -37,12 +41,12 @@ function generateArray (arrLength) {
     const j = Math.floor(Math.random() * (i + 1));
     [arr[i], arr[j]] = [arr[j], arr[i]];
   }
-  console.log('scrambelled arrary', arr);
+
   drawBars(arr);
   bubbleSort();
 }
 
-function drawBars (arr) {
+function drawBars (arr, arr2) {
   let c = document.getElementById("bubble");
   let ctx = c.getContext("2d");
   ctx.clearRect(0, 0, 1200, 625);
@@ -50,9 +54,12 @@ function drawBars (arr) {
   audio.play();
 
   for (let i = 0; i < arr.length; i++) {
-      ctx.strokeStyle = "blue";
+    if (!arr) {
+
+    } else {
       ctx.fillRect(box + (box * i * 2), box * 25, box, -box * arr[i]);
     }
+  }
 }
 
 // Sorter
@@ -69,31 +76,37 @@ function bubbleSort() {
         let temp = arr[i];
         arr[i] = arr[i + 1];
         arr[i + 1] = temp;
-        sorts ++;
         let arr2 = arr.slice();
         savedArray.push(arr2);
-        console.log(savedArray);
+        changedIndex.push(i);
+        sorts ++;
       }
     }
     if (sorts === 0) {
       sorted = true;
     }
   }
+  console.log(changedIndex);
 }
 
 function nextStep() {
-  currentStep ++;
   if (savedArray[currentStep]) {
+    currentStep ++;
     stepsOutput.innerHTML = currentStep;
     arr = savedArray[currentStep];
     drawBars(arr);
+  } else {
+
   }
 }
 
 function previousStep() {
-  currentStep --;
-  arr = savedArray[currentStep];
-  drawBars(arr);
+  if (savedArray[currentStep]) {
+    currentStep --;
+    stepsOutput.innerHTML = currentStep;
+    arr = savedArray[currentStep];
+    drawBars(arr);
+  }
 }
 
 function autoSort() {
@@ -104,4 +117,5 @@ function stopAutoSort() {
   clearInterval(sort);
 }
 generateArray(12);
+
 
--- bubble sorter/readme.md
+++ bubble sorter/readme.md
@@ -1,6 +1,5 @@
 # ToDO
 * highlight the bars that are swapping
-* ability to add custom numbers
 * ability to adjust the number of numbers sorted
 * ability to step backwards
 * track number of steps
